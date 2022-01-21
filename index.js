e4489b3e8299a4910f8685c841c350768b4da51b
Jeremias Storm <jerestorm@gmail.com>
2022-01-21 20:52:02 -0300

    portada casi lista (falta nav)

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const text = 
+`Soy estudiante Universitario en Desarrollo de Aplicaciones Informaticas de la UNICEN.
+
+Soy una persona proactiva, con un gran sentido de la responsabilidad, con vocación de servicio y capaz de generar valor agregado al equipo.
+
+Mi objetivo es adquirir experiencia laboral y consolidarme en un equipo de trabajo donde pueda desarrollarme y crecer profesionalmente.
+`;
+
+
+
+
